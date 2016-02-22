73d245c018aee7ec93c32bff9ceb03d812ded114
Albert Wang <albertyw@mit.edu>
2016-02-22 01:07:18 -0800

    Add navbar

--- albertyw.com/serve.py
+++ albertyw.com/serve.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.htm")
+
+@app.route("/resume")
+def resume():
+    return render_template("index.htm")
+
+@app.route("/projects")
+def projects():
+    return render_template("index.htm")
+
+@app.route("/contact")
+def contact():
+    return render_template("index.htm")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
