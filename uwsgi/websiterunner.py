063531273125ed2db75daf10ec53aedbe7d65d4b
Albert Wang <albertyw@mit.edu>
2016-02-21 16:27:06 -0800

    Add uWSGI and nginx config files

---
+++ uwsgi/websiterunner.py
@@ -0,0 +1,10 @@
+import os
+import sys
+tracker_path = os.path.dirname(os.path.realpath(__file__))+'/../albertyw.com/'
+sys.path.append(tracker_path)
+
+import monitor
+monitor.start(interval=1.0)
+#monitor.track(os.path.join(os.path.dirname(__file__), 'site.cf'))
+
+from serve import *
