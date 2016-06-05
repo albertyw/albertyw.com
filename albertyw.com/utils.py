6eb3b6aacbc80be2d2f2d8b8a194d1a8352216a6
Albert Wang <albertyw@mit.edu>
2016-06-04 22:59:46 -0700

    Prune note files

--- albertyw.com/utils.py
+++ albertyw.com/utils.py
@@ -1,12 +1,16 @@
 import json
 import os
 
+def prune_note_files(note_files):
+    files = [note_file for note_file in note_files if '~' not in note_file]
+    return files
 
 def get_note_files():
     current_directory = os.path.dirname(os.path.realpath(__file__))
     notes_directory = os.path.join(current_directory, 'notes')
     files = os.listdir(notes_directory)
     files.sort()
+    files = prune_note_files(files)
     files = [os.path.join(notes_directory, note_file) for note_file in files]
     return files
 
