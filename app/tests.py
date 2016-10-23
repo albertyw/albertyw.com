93e85e38fd9ff26c5e467c994573279cbea5945f
Albert Wang <albertyw@mit.edu>
2016-10-22 18:28:27 -0700

    Add more tests

--- app/tests.py
+++ app/tests.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
+import tempfile
 import unittest
 
-import serve
+import serve, utils
 
 
 class PageCase(unittest.TestCase):
@@ -35,6 +36,26 @@ class PageCase(unittest.TestCase):
         response = self.app.get(path)
         self.assertEqual(response.status_code, 200)
         self.assertIn(string, response.get_data())
+
+
+class UtilCase(unittest.TestCase):
+    def test_get_malformed_note(self):
+        note = b''
+        note_file = tempfile.NamedTemporaryFile()
+        note_file.write(note)
+        note = utils.get_note(note_file.name, None)
+        self.assertEqual(note, None)
+        note_file.close()
+
+    def test_prune_tilde_notes(self):
+        note_files = ['asdf', 'asdf~']
+        note_files = utils.prune_note_files(note_files)
+        self.assertEqual(note_files, ['asdf'])
+
+    def test_prune_dotfile_notes(self):
+        note_files = ['asdf', '.asdf']
+        note_files = utils.prune_note_files(note_files)
+        self.assertEqual(note_files, ['asdf'])
 
 
 if __name__ == '__main__':
