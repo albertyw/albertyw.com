01a80a8056012e8a2294d031313e18f845948f40
Albert Wang <git@albertyw.com>
2017-09-02 21:57:30 -0700

    Add Note class

--- app/tests/test_note.py
+++ app/tests/test_note.py
@@ -1,7 +1,26 @@
 import tempfile
 import unittest
 
+import pytz
+
 import note_util
+
+
+class TestNote(unittest.TestCase):
+    def setUp(self):
+        self.note = note_util.Note()
+
+    def test_parse_time(self):
+        time = 1504334092
+        self.note.parse_time(time, pytz.timezone('America/Los_Angeles'))
+        self.assertEqual(self.note.time.year, 2017)
+        self.assertEqual(self.note.time.month, 9)
+        self.assertEqual(self.note.time.day, 1)
+
+    def test_parse_note(self):
+        note = ['[x](y)']
+        self.note.parse_note(note)
+        self.assertEqual(self.note.note, '<p><a href="y">x</a></p>\n')
 
 
 class UtilCase(unittest.TestCase):
