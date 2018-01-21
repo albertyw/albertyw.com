8c5125000f16b2b11aa6eb2cb917be2fa87040fd
Albert Wang <git@albertyw.com>
2018-01-20 20:17:32 -0800

    Fixes

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -5,4 +5,4 @@ import dotenv
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 env_path = os.path.join(current_path, '..', '..', '.env')
-dotenv.read_dotenv(env_path)
+dotenv.load_dotenv(env_path)
