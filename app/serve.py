3884a55f853aaf3e2c78c7672ebb7584ce89c425
Albert Wang <git@albertyw.com>
2018-07-14 16:42:00 -0700

    Update to base-flask v2.4.1

--- app/serve.py
+++ app/serve.py
@@ -7,12 +7,12 @@ from flask import (
 )
 from flask_assets import Environment, Bundle
 from flask_sitemap import Sitemap
-from syspath import git_root, get_git_root  # NOQA
+from syspath import git_root
 
 from app.routes import handlers, sitemap_urls
 
 import dotenv
-dotenv.load_dotenv(os.path.join(get_git_root(), '.env'))
+dotenv.load_dotenv(os.path.join(git_root.path, '.env'))
 
 
 app = Flask(__name__)
