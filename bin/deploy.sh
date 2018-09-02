3435736aea4bcfdea58cdc4e2abc41f4469ff7cd
Albert Wang <git@albertyw.com>
2018-09-01 23:37:31 -0700

    Set name on running docker container

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,11 +10,11 @@ git pull
 
 # Build and start container
 docker build -t albertyw.com:production .
-docker stop $(docker ps -q --filter ancestor=albertyw.com:production ) || echo
-docker run --detach --restart always -p 127.0.0.1:5000:5000 albertyw.com:production
-
-# Cleanup dockeer
+docker stop albertyw.com || echo
 docker container prune -f
+docker run --detach --restart always -p 127.0.0.1:5000:5000 --name albertyw.com albertyw.com:production
+
+# Cleanup docker
 docker image prune -f
 
 # Update nginx
