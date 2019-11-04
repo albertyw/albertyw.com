8da89aacf70d89f0f6ecdaff38ab91cbe4c64b6f
Albert Wang <git@albertyw.com>
2019-11-03 16:37:49 -0800

    Make sure pdf is loaded at the correct time

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -1,3 +1,5 @@
 if(document.getElementById("resume") !== null) {
-  PDFObject.embed("/static/gen/resume.pdf", "#resume");
+  $(() => {
+    PDFObject.embed("/static/gen/resume.pdf", "#resume");
+  });
 }
