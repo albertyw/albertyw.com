2b332a109512b758c250677173b388edaaa26629
Albert Wang <git@albertyw.com>
2020-02-29 01:33:53 -0800

    Rename docker entities

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,22 +19,22 @@ fi
 
 # Build and start container
 docker pull "$(grep FROM Dockerfile | awk '{print $2}')"
-docker build -t "albertyw.com:$ENV" .
-docker network inspect "albertyw.com" &>/dev/null ||
-    docker network create --driver bridge "albertyw.com"
-docker stop "albertyw.com" || true
+docker build -t "albertyw:$ENV" .
+docker network inspect "albertyw" &>/dev/null ||
+    docker network create --driver bridge "albertyw"
+docker stop "albertyw" || true
 docker container prune --force --filter "until=168h"
 docker image prune --force --filter "until=168h"
 docker volume prune --force
-docker container rm "albertyw.com" || true
+docker container rm "albertyw" || true
 docker run \
     --detach \
     --restart=always \
     --publish="127.0.0.1:5000:5000" \
-    --network="albertyw.com" \
+    --network="albertyw" \
     --mount type=bind,source="$(pwd)"/app/static,target=/var/www/app/app/static \
     --mount type=bind,source="$(pwd)"/logs,target=/var/www/app/logs \
-    --name "albertyw.com" "albertyw.com:$ENV"
+    --name "albertyw" "albertyw:$ENV"
 
 if [ "$ENV" = "production" ]; then
     # Cleanup docker
