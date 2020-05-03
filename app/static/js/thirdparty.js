691b9b851f7158faea9b810e55543e616d79eb98
Albert Wang <git@albertyw.com>
2020-05-02 20:42:24 -0700

    Fix typo

--- app/static/js/thirdparty.js
+++ app/static/js/thirdparty.js
@@ -28,8 +28,8 @@ function setupVarsnap() {
     env: process.env.ENV,
     producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
     consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
-  };
-});
+  });
+};
 
 module.exports = {
   rollbar: setupRollbar,
