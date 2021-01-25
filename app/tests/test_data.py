46af2be6f74697a4fe1b02d208b1e01e9dc98b74
Albert Wang <git@albertyw.com>
2021-01-24 21:38:47 -0800

    Add tests

--- app/tests/test_data.py
+++ app/tests/test_data.py
@@ -22,3 +22,23 @@ class TestProjects(unittest.TestCase):
         projects1 = data.get_projects()
         projects2 = data.get_projects()
         self.assertEqual(projects1, projects2)
+
+
+class TestShelf(unittest.TestCase):
+    def setUp(self) -> None:
+        self.original_cache = util.SHOULD_CACHE
+        util.SHOULD_CACHE = True
+
+    def tearDown(self) -> None:
+        util.SHOULD_CACHE = self.original_cache
+
+    def test_load(self) -> None:
+        shelf = data.Shelf.load()
+        self.assertNotEqual(shelf.data, {})
+        self.assertIn('Technology', shelf.data)
+        self.assertIn('link', shelf.data['Technology'][0])
+
+    def test_get_shelf(self) -> None:
+        shelf1 = data.get_shelf()
+        shelf2 = data.get_shelf()
+        self.assertEqual(shelf1, shelf2)
