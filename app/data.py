0df292fbb34a66ee66fce919ea63b68a5f9eff1a
Albert Wang <git@albertyw.com>
2021-01-26 23:44:15 -0800

    Set up data structures for parsing projects

--- app/data.py
+++ app/data.py
@@ -7,22 +7,78 @@ from app.util import cached_function
 
 class Projects():
     def __init__(self) -> None:
-        self.data: Dict[str, Dict[str, Dict[str, str]]] = {}
+        self.languages: List[Language] = []
 
     @staticmethod
-    def load() -> 'Projects':
+    def load_from_file() -> 'Projects':
         current_directory = os.path.dirname(os.path.realpath(__file__))
         path = os.path.join(current_directory, 'data', 'projects.json')
         with open(path, 'r') as handle:
-            project_data = handle.read()
+            data = handle.read()
+        parsed_data = json.loads(data)
+        return Projects.load(parsed_data)
+
+    @staticmethod
+    def load(data: Dict[str, Dict[str, Dict[str, str]]]) -> 'Projects':
         projects = Projects()
-        projects.data = json.loads(project_data)
+        for key, value in data.items():
+            language = Language.load(key, value)
+            projects.languages.append(language)
         return projects
+
+
+class Language():
+    def __init__(self) -> None:
+        self.name: str = ''
+        self.projects: List[Project] = []
+
+    @staticmethod
+    def load(key: str, data: Dict[str, Dict[str, str]]) -> 'Language':
+        language = Language()
+        language.name = key
+        for key, value in data.items():
+            project = Project.load(key, value)
+            language.projects.append(project)
+        return language
+
+
+class Project():
+    def __init__(self) -> None:
+        self.name: str = ''
+        self.description: str = ''
+        self.github: str = ''
+        self.rubygems: str = ''
+        self.pypi: str = ''
+        self.npm: str = ''
+        self.web: str = ''
+
+    @staticmethod
+    def load(key: str, data: Dict[str, str]) -> 'Project':
+        project = Project()
+        project.name = key
+        project.description = data.get('description', '')
+        project.github = data.get('github', '')
+        project.rubygems = data.get('rubygems', '')
+        project.pypi = data.get('pypi', '')
+        project.npm = data.get('npm', '')
+        project.web = data.get('web', '')
+        return project
+
+    def links(self) -> Dict[str, str]:
+        links: Dict[str, str] = {
+            'github': self.github,
+            'rubygems': self.rubygems,
+            'pypi': self.pypi,
+            'npm': self.npm,
+            'web': self.web,
+        }
+        links = dict([(k, v) for k, v in links.items() if v])
+        return links
 
 
 @cached_function
 def get_projects() -> Projects:
-    loaded_projects = Projects.load()
+    loaded_projects = Projects.load_from_file()
     return loaded_projects
 
 
