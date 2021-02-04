afb54e2ec9164072b9fcc3f25706bff59f6dda25
Albert Wang <git@albertyw.com>
2021-02-03 22:56:59 -0800

    Delete basic server setup instructions

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,19 +8,9 @@
 set -exuo pipefail
 IFS=$'\n\t'
 
-# Setup server
-sudo hostnamectl set-hostname "albertyw.com"
-
 # Clone repository
 cd ~
 git clone "git@github.com:albertyw/albertyw.com"
-
-# Setup up docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker "${USER}"
 
 # Set up directory structures
 ln -s .env.production .env
