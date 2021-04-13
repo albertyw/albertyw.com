75c52913f886d70ad11d96dcfcf154c03c9163c3
Albert Wang <git@albertyw.com>
2021-04-13 14:04:53 +0800

    Refactor tests

--- static/js/test/test.js
+++ static/js/test/test.js
@@ -1,10 +1,7 @@
 const expect = require('chai').expect;
 const varsnap = require('varsnap');
 
-const thirdparty = require('../thirdparty');
 require('../fibonacci');
-
-thirdparty.setupVarsnap();
 
 context('Varsnap', function() {
   this.timeout(30 * 1000);
