2429a5f2d8be9352ffb61f33acb6a31d4f611965
Albert Wang <git@albertyw.com>
2021-07-20 00:22:55 -0700

    Configure markdown lint

---
+++ app/tests/mdl_style.rb
@@ -0,0 +1,8 @@
+all
+rule 'MD029', style: :ordered
+exclude_rule 'MD013'  # Temporary exclusion.  TODO: remove
+exclude_rule 'MD041'  # Notes are displayed with headers added in separately
+exclude_rule 'MD002'  # Ibid
+exclude_rule 'MD030'  # Rule conflicts with style guide
+exclude_rule 'MD010'  # False positive hard tabs visible in code fences
+exclude_rule 'MD026'  # False positive on app/notes/20180711-0403.md
