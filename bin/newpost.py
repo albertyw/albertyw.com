fc53bea92377b99192bdf7ac30bed76b2c904b66
Albert Wang <git@albertyw.com>
2021-12-27 00:15:04 -0500

    Modularize newpost.py

--- bin/newpost.py
+++ bin/newpost.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
+"""
+This script generates a new note
+"""
 
 import calendar
 import datetime
 import os
 import subprocess
 
+
 EDITOR = os.environ.get('EDITOR', 'vim')
 
-current_time = datetime.datetime.utcnow()
 
-current_directory = os.path.dirname(os.path.realpath(__file__))
-notes_directory = os.path.join(
-    current_directory, '..', 'app', 'notes')
-note_filename = current_time.strftime('%Y%m%d-%H%M.md')
-note_path = os.path.join(notes_directory, note_filename)
-note_path = os.path.normpath(note_path)
+def generate_note() -> str:
+    current_time = datetime.datetime.utcnow()
 
-timestamp = calendar.timegm(current_time.utctimetuple())
-note = "title\n\nslug\n\n%s\n\nnote\n" % timestamp
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    notes_directory = os.path.join(
+        current_directory, '..', 'app', 'notes')
+    note_filename = current_time.strftime('%Y%m%d-%H%M.md')
+    note_path = os.path.join(notes_directory, note_filename)
+    note_path = os.path.normpath(note_path)
 
-with open(note_path, 'w') as note_handle:
-    note_handle.write(note)
+    timestamp = calendar.timegm(current_time.utctimetuple())
+    note = "title\n\nslug\n\n%s\n\nnote\n" % timestamp
 
-print('Note template written to %s' % note_path)
-subprocess.call([EDITOR, note_path])
+    with open(note_path, 'w') as note_handle:
+        note_handle.write(note)
+    return note_path
+
+
+def edit_note(note_path: str) -> None:
+    subprocess.call([EDITOR, note_path])
+
+
+if __name__ == '__main__':
+    note_path = generate_note()
+    print('Note template written to %s' % note_path)
+    edit_note(note_path)
