5ca5ea22ba6f9664461f3e10124a155b1e3c0a7d
Albert Wang <git@albertyw.com>
2021-12-27 00:39:43 -0500

    Support generating references with newpost

--- bin/newpost.py
+++ bin/newpost.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-"""
-This script generates a new note
-"""
-
+import argparse
 import calendar
 import datetime
 import os
 from pathlib import Path
 import subprocess
+from typing import Optional, cast
 
 from syspath import get_git_root
 
 
+DESCRIPTION = "This script generates a new note or reference"
 EDITOR = os.environ.get('EDITOR', 'vim')
 
 
-def generate_note() -> Path:
+def get_reference_slug() -> Optional[str]:
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('reference_slug', nargs='?', type=str)
+    params = parser.parse_args()
+    reference_slug = cast(Optional[str], params.reference_slug)
+    return reference_slug
+
+
+def generate_note(reference_name: Optional[str]) -> Path:
     current_time = datetime.datetime.utcnow()
 
-    note_filename = current_time.strftime('%Y%m%d-%H%M.md')
-    note_path = get_git_root() / 'app' / 'notes' / note_filename
+    if reference_name:
+        note_filename = f'{reference_name}.md'
+        note_path = get_git_root() / 'app' / 'reference' / note_filename
+    else:
+        note_filename = current_time.strftime('%Y%m%d-%H%M.md')
+        note_path = get_git_root() / 'app' / 'notes' / note_filename
 
     timestamp = calendar.timegm(current_time.utctimetuple())
     note = "title\n\nslug\n\n%s\n\nnote\n" % timestamp
@@ -34,6 +45,7 @@ def edit_note(note_path: Path) -> None:
 
 
 if __name__ == '__main__':
-    note_path = generate_note()
+    reference_name = get_reference_slug()
+    note_path = generate_note(reference_name)
     print('Note template written to %s' % note_path)
     edit_note(note_path)
