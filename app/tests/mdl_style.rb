67800db6670add5b266dd62d3ae3e7d89d832fd2
Albert Wang <git@albertyw.com>
2022-02-13 21:29:39 -0500

    Allow html in notes

--- app/tests/mdl_style.rb
+++ app/tests/mdl_style.rb
@@ -6,3 +6,4 @@ exclude_rule 'MD002'  # Ibid
 exclude_rule 'MD030'  # Rule conflicts with style guide
 exclude_rule 'MD010'  # False positive hard tabs visible in code fences
 exclude_rule 'MD026'  # False positive on app/notes/20180711-0403.md
+exclude_rule 'MD033'  # Allow HTML in notes, needed for things that markdown doesn't support
