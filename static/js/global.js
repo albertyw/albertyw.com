7adb3d63df5eb50624df2a771355c6c0b5cf51bf
Albert Wang <git@albertyw.com>
2025-01-19 19:16:49 -0800

    Bold navbar current navbar link

--- static/js/global.js
+++ static/js/global.js
@@ -2,3 +2,14 @@ import '../css/global.css';
 import '../css/syntax.css';
 import { watchKeyboardEvents } from './navigation.js';
 watchKeyboardEvents();
+
+function navbarBold() {
+  const currentPage = window.location.pathname;
+  console.log(currentPage);
+  for (const link of document.getElementsByClassName('nav-link')) {
+    if (link.getAttribute('href') === currentPage) {
+      link.classList.add('active');
+    }
+  }
+}
+navbarBold();
