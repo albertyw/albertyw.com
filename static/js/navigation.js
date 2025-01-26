edfedb8315b32754094c73b609ba14acd9061a11
Albert Wang <git@albertyw.com>
2025-01-26 01:26:43 -0800

    Underline text when g is selected

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -7,6 +7,24 @@ const navigationOptions = {
   'p': '/projects',
   'r': '/reference',
 };
+const navigationOptionsFlipped = Object.fromEntries(Object.entries(navigationOptions).map(a => a.reverse()))
+
+function underlineText() {
+  const links = document.querySelectorAll('.navbar a');
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    const href = link.getAttribute('href');
+    const letter = navigationOptionsFlipped[href];
+    let text = link.innerHTML;
+    for (let j = 0; j < text.length; j++) {
+      if (text[j].toLowerCase() === letter) {
+        text = text.replace(text[j], '<u>' + text[j] + '</u>');
+        break;
+      }
+    }
+    link.innerHTML = text;
+  }
+}
 
 function processKeyHistory() {
   if (keyHistory[0] !== 'g') {
@@ -14,6 +32,7 @@ function processKeyHistory() {
     return;
   }
   if (keyHistory.length < 2) {
+    underlineText();
     return;
   }
   const action = keyHistory[1];
