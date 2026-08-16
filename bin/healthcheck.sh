06ac8175af9b98f4721013b74a9ee7ec713e98df
Albert Wang <git@albertyw.com>
2026-08-16 00:30:10 -0700

    Update to base-flask v3.23.5

--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -6,7 +6,9 @@ IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"/..
 
+set +x  # Do not print contents of .env
 source .env
+set -x
 
 if [ -z "$SERVER_NAME" ]; then
     exit 0
@@ -14,4 +16,4 @@ fi
 curl \
     --fail \
     --header "Host: $SERVER_NAME" \
-    "localhost:5000/health"
+    "http://localhost:5000/health"
